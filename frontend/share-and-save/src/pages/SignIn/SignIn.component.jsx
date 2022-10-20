046663ffdafc7fe04eecca054290081f35eaac3e
Chiahsuan Chou <belinda05021992@gmail.com>
2022-10-20 17:38:37 +0800

    feat: implement facebook sign-in functionality

--- frontend/share-and-save/src/pages/SignIn/SignIn.component.jsx
+++ frontend/share-and-save/src/pages/SignIn/SignIn.component.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 import { ContentWrapper } from "../SignUp/SignUp.styles";
 import SignInForm from "../../components/SignInForm/SignInForm.component";
+import FacebookLogin from "react-facebook-login";
+import "./facebook.styles.scss";
 
 const SignInPage = () => {
   const handleCredentialResponse = (response) => {
     console.log("Encoded JWT ID token: " + response.credential);
+    //TODO: post request
   };
 
   useEffect(() => {
@@ -21,11 +24,36 @@ const SignInPage = () => {
     );
   }, []);
 
+  // Facebook sing-in
+  let username = "";
+  let userEmail = "";
+
+  function responseFacebook(values) {
+    const { name, email } = values;
+    username = name;
+    userEmail = email;
+  }
+
+  function componentClicked() {
+    console.log("username=>", username);
+    console.log("userEmail=>", userEmail);
+    //TODO: post request
+  }
+
   return (
     <ContentWrapper vertical width="450px">
       <h2>登入</h2>
       <SignInForm />
-      <div id="google-login-button" style={{marginTop: '60px'}}></div>
+      <div id="google-login-button" style={{ marginTop: "60px" }}></div>
+      <FacebookLogin
+        appId="642590437217919"
+        autoLoad={true}
+        fields="name,email,picture"
+        onClick={componentClicked}
+        callback={responseFacebook}
+        cssClass="my-facebook-button"
+        icon="fa-facebook"
+      />
     </ContentWrapper>
   );
 };
