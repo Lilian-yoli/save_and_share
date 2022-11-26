abbc9e06451ee7ae9b9861224e0ea5eb0fdb3c98
LilianYang <lilianyang@emq.com>
2022-11-26 11:47:23 +0800

    connect react to local nodejs server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.scss";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "../src/components/Navbar/Navbar.component";
+import Navbar from "./components/Navbar/Navbar.component";
 import HomePage from "./pages/Home/Home.component";
 import SearchPage from "./pages/Search/Search.component";
 import SharePage from "./pages/Share/Share.component";
