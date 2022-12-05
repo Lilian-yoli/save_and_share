03a79ed69e3516380fa410550439af0fea1279cb
LilianYang <lilianyang@emq.com>
2022-12-05 17:36:33 +0800

    add swagger_user.json for user api docs

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const express = require("express");
 const app = express();
 const { PORT, API_VERSION } = process.env;
 const bodyParser = require("body-parser");
+const swaggerUi = require("swagger-ui-express");
+const swaggerFile = require("./swagger_user.json");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend: true }));
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(function (req, res, next) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
