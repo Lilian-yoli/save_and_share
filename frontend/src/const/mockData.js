14485a48f30e54685456af2cba509e4c4afa30dd
Chiahsuan Chou <belinda05021992@gmail.com>
2022-12-09 17:12:57 +0800

    feat: complete my-share page layout

---
+++ frontend/src/const/mockData.js
@@ -0,0 +1,54 @@
+export const mockData = [
+  {
+    id: 1,
+    foodName: "貝果",
+    expiryDate: "2022/11/22",
+    description: "芝麻口味",
+    price: 500,
+    pricePerPortion: 100,
+    meetUpDate: "2022-11-14",
+    meetUpTime: "12:00",
+    totalPortions: 5,
+    takenPortions: 1,
+    unit: '包'
+  },
+  {
+    id: 2,
+    foodName: "貝果",
+    expiryDate: "2022/11/22",
+    description: "位前尺時國息婆父皮七日二犬寫百長位前尺時國息婆父皮七日二犬寫百長位前尺時國息婆父皮七日二犬寫百長，皮七日二犬寫百長皮七日二犬寫百長走几交信刃媽有根紅畫學聽內進耳星來蛋自？固空寸着秋晚十直六方個弟禾，木香欠珠工像美：她害問哥少弟定老呢：首科園事央親亮又造又歌哪，走學可屋鴨書借冒浪媽息門裝你訴昌寺。",
+    price: 500,
+    meetUpDate: "2022-11-14",
+    meetUpTime: "09:18",
+    pricePerPortion: 100,
+    totalPortions: 4,
+    takenPortions: 3,
+    unit: '包'
+  },
+  {
+    id: 2,
+    foodName: "貝果",
+    expiryDate: "2022/11/22",
+    description: "位前尺時國息婆父皮七日二犬寫百長位前尺時國息婆父皮七日二犬寫百長位前尺時國息婆父皮七日二犬寫百長，皮七日二犬寫百長皮七日二犬寫百長走几交信刃媽有根紅畫學聽內進耳星來蛋自？固空寸着秋晚十直六方個弟禾，木香欠珠工像美：她害問哥少弟定老呢：首科園事央親亮又造又歌哪，走學可屋鴨書借冒浪媽息門裝你訴昌寺。",
+    price: 500,
+    meetUpDate: "2022-11-14",
+    meetUpTime: "09:18",
+    pricePerPortion: 100,
+    totalPortions: 4,
+    takenPortions: 3,
+    unit: '包'
+  },
+  {
+    id: 2,
+    foodName: "貝果",
+    expiryDate: "2022/11/22",
+    description: "位前尺時國息婆父皮七日二犬寫百長位前尺時國息婆父皮七日二犬寫百長位前尺時國息婆父皮七日二犬寫百長，皮七日二犬寫百長皮七日二犬寫百長走几交信刃媽有根紅畫學聽內進耳星來蛋自？固空寸着秋晚十直六方個弟禾，木香欠珠工像美：她害問哥少弟定老呢：首科園事央親亮又造又歌哪，走學可屋鴨書借冒浪媽息門裝你訴昌寺。",
+    price: 500,
+    meetUpDate: "2022-11-14",
+    meetUpTime: "09:18",
+    pricePerPortion: 100,
+    totalPortions: 4,
+    takenPortions: 3,
+    unit: '包'
+  },
+];
