fdbe60b01d79c03225fb11fd2e225692eb691091
Lilian Yang <77181372+Lilian-yoli@users.noreply.github.com>
2022-12-09 10:43:48 +0800

    SSB-0010: set Access-Control-Allow-Origin to frontend host (#27)

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const { PORT, API_VERSION } = process.env;
+const { PORT, API_VERSION, FRONTEND_HOST } = process.env;
 const bodyParser = require("body-parser");
 const swaggerUi = require("swagger-ui-express");
 const swaggerFile = require("./swagger_user.json");
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extend: true }));
 app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Origin", FRONTEND_HOST);
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   res.setHeader("Access-Control-Allow-Credentials", true);
