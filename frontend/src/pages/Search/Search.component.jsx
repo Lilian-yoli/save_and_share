d22b4d2e482d0d97b6cb8dfec781c75854a6a4ff
LilianYang <lilianyang@emq.com>
2022-12-14 21:17:47 +0800

    delete redundant client and frontend folder

--- frontend/src/pages/Search/Search.component.jsx
+++
@@ -1,9 +0,0 @@
-const SearchPage = () => {
-  return (
-    <>
-      <p>想要加入分購嗎？</p>
-    </>
-  );
-};
-
-export default SearchPage;
--- frontend/src/utils/errorMessages.js
+++
@@ -1,9 +0,0 @@
-const errors = {
-  "Duplicated Email.": "此信箱已註冊過",
-  "Invalid information.": "帳號或密碼有誤",
-};
-
-export default function convertErrorMessages(error) {
-  const { errorMsg } = error.response.data.error;
-  return errors[errorMsg];
-}
