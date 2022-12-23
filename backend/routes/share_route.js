fabe850134ae245201ef0a713e225d45dafed369
LilianYang <lilianyang@emq.com>
2022-12-23 16:42:33 +0800

    SSB-0013: add shareSearchFlow

--- backend/routes/share_route.js
+++ backend/routes/share_route.js
@@ -1,9 +1,14 @@
 const router = require("express").Router();
 const { wrapAsync, authentication } = require("../utils");
-const { shareLaunchFlow } = require("../controllers/share_controller");
+const {
+  shareLaunchFlow,
+  shareSearchFlow,
+} = require("../controllers/share_controller");
 
 router
   .route("/share/share-launch")
   .post(authentication(), wrapAsync(shareLaunchFlow));
+
+router.route("/share/share-search").post(wrapAsync(shareSearchFlow));
 
 module.exports = router;
