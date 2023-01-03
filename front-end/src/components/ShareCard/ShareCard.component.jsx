7b41b49d04e211686a71f96035761380ee9067d9
Chiahsuan Chou <belinda05021992@gmail.com>
2023-01-03 10:12:26 +0800

    feat: get share list launched by the user

--- front-end/src/components/ShareCard/ShareCard.component.jsx
+++ front-end/src/components/ShareCard/ShareCard.component.jsx
@@ -2,34 +2,36 @@ import LocalPizzaIcon from '@mui/icons-material/LocalPizza';
 import LocalPizzaOutlinedIcon from '@mui/icons-material/LocalPizzaOutlined';
 import { ShareCardWrapper, ContentWrapper } from './ShareCard.styles';
 import Button from "../../components/Button/Button.component";
+import dayjs from "dayjs";
 
 
 const ShareCard = ({ info, shadow, cancelable }) => {
 
-  let { foodName, expiryDate, description, pricePerPortion, unit, totalPortions, takenPortions } = info
+  let { name, expiry_date, description, price, unit_description, total_portions, total_taken_portions } = info;
+  const pricePerPortion = Math.round(price / total_portions);
 
   function makeArray(length) {
     return Array.from({ length: length }, (_, i) => i);
   }
 
-  const remainingPortions = makeArray(totalPortions - takenPortions);
-  takenPortions = makeArray(takenPortions);
+  const remainingPortions = makeArray(total_portions - total_taken_portions);
+  total_taken_portions = makeArray(total_taken_portions);
 
   return (
     <>
       <ShareCardWrapper shadow={shadow}>
         <ContentWrapper>
-          <strong>食物名稱:<span>{foodName}</span></strong>
-          <strong>價格:<span>NTD {pricePerPortion} / {unit}</span></strong>
+          <strong>食物名稱: <span>{name}</span></strong>
+          <strong>價格: <span>NTD {pricePerPortion} / {unit_description}</span></strong>
         </ContentWrapper>
         <ContentWrapper direction="column">
           <strong>食物描述: </strong>
           <p>{description}</p>
         </ContentWrapper>
         <ContentWrapper>
-          <strong>食物到期日:<span>{expiryDate}</span></strong>
+          <strong>食物到期日: <span>{dayjs(expiry_date).format('YYYY年M月D日')}</span></strong>
           <div>
-            {takenPortions.map((_, index) => < LocalPizzaIcon key={index} />)}
+            {total_taken_portions.map((_, index) => < LocalPizzaIcon key={index} />)}
             {remainingPortions.map((_, index) => (<LocalPizzaOutlinedIcon key={index} />))}
           </div>
         </ContentWrapper>
--- front-end/src/pages/MyShare/MyShare.component.jsx
+++ front-end/src/pages/MyShare/MyShare.component.jsx
@@ -1,14 +1,24 @@
 import ShareCard from "../../components/ShareCard/ShareCard.component"
-import { mockData } from '../../const/mockData';
 import { PageWrapper } from "../../components/Layout/PageWrapper.styles";
+import { useState, useEffect } from 'react';
+import { GET } from "../../utils/API";
 
 
 const MySharePage = () => {
+  const [shareList, setShareList] = useState();
+
+  useEffect(() => {
+    const fetchMyShareList = async () => {
+      const { data: { data } } = await GET('/share/personal-launch');
+      setShareList(data);
+    }
+    fetchMyShareList();
+  }, [])
 
   return (
     <PageWrapper>
       <h2>發起的分購</h2>
-      {mockData.map((data, index) => <ShareCard key={index} info={data} shadow="none" cancelable={true} />)}
+      {shareList?.length > 0 && shareList.map((data, index) => <ShareCard key={index} info={data} shadow="none" cancelable={true} />)}
     </PageWrapper>
   )
 }
