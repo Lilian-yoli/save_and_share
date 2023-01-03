37557e97ea17d87d1a3476b2ef2b898ed4d01586
Chiahsuan Chou <belinda05021992@gmail.com>
2023-01-03 15:26:08 +0800

    feat: finish my-purchase page's functionality

--- front-end/src/pages/MyPurchase/MyPurchase.component.jsx
+++ front-end/src/pages/MyPurchase/MyPurchase.component.jsx
@@ -1,14 +1,27 @@
 import ShareCard from "../../components/ShareCard/ShareCard.component"
-import { mockData } from '../../const/mockData';
 import { PageWrapper } from "../../components/Layout/PageWrapper.styles";
-
+import { useEffect, useState } from "react";
+import { GET, PUT } from "../../utils/API";
 
 const MyPurchasePage = () => {
+  const [purchaseList, setPurchaseList] = useState();
+
+  useEffect(() => {
+    const fetchMyPurchaseList = async () => {
+      const { data: { data } } = await GET('/share/personal-join');
+      setPurchaseList(data);
+    }
+    fetchMyPurchaseList();
+  }, [])
+
+  const deletePurchase = async (id) => {
+    await PUT('/share/delete-joined-share', null, { params: { matchId: id } })
+  }
 
   return (
     <PageWrapper>
       <h2>已加入的分購</h2>
-      {mockData.map((data, index) => <ShareCard key={index} info={data} shadow="none" cancelable={true} />)}
+      {purchaseList?.length && purchaseList.map((data, index) => <ShareCard key={index} info={data} shadow="none" cancelable={true} action={() => deletePurchase(data.share_id)} />)}
     </PageWrapper>
   )
 }
