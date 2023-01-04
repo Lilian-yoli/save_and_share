56dcb59b8ff96dce0bdbd1e196eca7d7daf7f4f9
Chiahsuan Chou <belinda05021992@gmail.com>
2023-01-04 15:17:29 +0800

    feat: complete detail page

---
+++ front-end/src/pages/SearchDetail/SearchDetail.component.jsx
@@ -0,0 +1,33 @@
+import { useParams } from "react-router-dom"
+import { GET } from "../../utils/API";
+import JoinForm from "../../components/JoinForm/JoinForm-component";
+import { SearchDetailWrapper } from "./SearchDetail.style";
+import { useEffect, useState } from "react";
+import Map from "../../components/Map/Map.component";
+
+const SearchDetail = () => {
+  const [data, setData] = useState();
+
+  const { id } = useParams()
+
+  useEffect(() => {
+    const fetchDetialById = async (id) => {
+      const { data: { data } } = await GET('/share/share-detail', { params: { shareId: id } });
+      setData(data)
+    }
+    fetchDetialById(id);
+  }, [])
+
+
+  return (
+    <SearchDetailWrapper>
+      <JoinForm shareDetail={data} share_id={id} />
+      <div>
+        <Map location={data?.location} />
+      </div>
+    </SearchDetailWrapper>
+  )
+}
+
+
+export default SearchDetail;
