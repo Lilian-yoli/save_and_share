7cbd62ce05bad144af11c7666c8182605d818451
LilianYang <lilianyang@emq.com>
2023-01-22 21:47:19 +0800

    SSB-0025: add two presigned url api to swagger doc

--- backend/S3.js
+++ backend/S3.js
@@ -1,5 +1,6 @@
 require("dotenv").config();
 const AWS = require("aws-sdk");
+const log = require("npmlog");
 const credentials = {
   accessKeyId: process.env.S3_ACCESS_KEY,
   secretAccessKey: process.env.S3_SECRET_KEY,
@@ -13,20 +14,30 @@ AWS.config.update({
 const s3 = new AWS.S3();
 
 const presignedGETURL = async (filename) => {
-  return s3.getSignedUrl("getObject", {
-    Bucket: "ss-shared-foods-picture",
-    Key: filename,
-    Expires: 600,
-  });
+  try {
+    return s3.getSignedUrl("getObject", {
+      Bucket: "ss-shared-foods-picture",
+      Key: filename,
+      Expires: 600,
+    });
+  } catch (error) {
+    log.error({ presignedGETURL: error });
+    throw error;
+  }
 };
 
 const presignedImagePUTURL = async (filename) => {
-  return s3.getSignedUrl("putObject", {
-    Bucket: "ss-shared-foods-picture",
-    Key: filename,
-    Expires: 600,
-    ContentType: "image",
-  });
+  try {
+    return s3.getSignedUrl("putObject", {
+      Bucket: "ss-shared-foods-picture",
+      Key: filename,
+      Expires: 600,
+      ContentType: "image",
+    });
+  } catch (error) {
+    log.error({ presignedImagePUTURL: error });
+    throw error;
+  }
 };
 
 module.exports = {
