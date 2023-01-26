3a3e912fd37bb0f58d576c4d0f78048651b4aa2f
LilianYang <lilianyang@emq.com>
2023-01-26 15:32:59 +0800

    SSB-0031: add comment in socket file

--- backend/controllers/socket_controller.js
+++ backend/controllers/socket_controller.js
@@ -1,3 +1,6 @@
+// TODO: apply saveChatMsgToDB in socket event
+const { saveChatMsgToDB } = require("../models/chat_model");
+
 // This is a file to manage socket event
 const socketCon = (io) => {
   io.on("connection", (socket) => {
