562a19a4184cd3b3666d0f7dabadde1ecaa4c6a0
Lilian Yang <77181372+Lilian-yoli@users.noreply.github.com>
2023-01-26 13:55:28 +0800

    SSB-0030: use fn saveChatMsgToDB to save chatting message to db (#63)

--- backend/models/chat_model.js
+++ backend/models/chat_model.js
@@ -19,6 +19,37 @@ const selectMessagesByRoom = async (room) => {
   }
 };
 
+const saveChatMsgToDB = async ({
+  sender_id,
+  receiver_id,
+  message,
+  room,
+  send_at,
+}) => {
+  try {
+    const insertedQuery =
+      "INSERT INTO chat_info(receiver_id, sender_id, message, send_at, updated, room, status) VALUES($1, $2, $3, $4, $5, $6, $7)";
+    const insertedResult = await pgsqlPool
+      .query(insertedQuery, [
+        sender_id,
+        receiver_id,
+        message,
+        send_at,
+        send_at,
+        room,
+        "active",
+      ])
+      .then((result) => {
+        console.log({ insertedResult: result.rowCount });
+        return result.rowCount;
+      });
+    return { data: "OK" };
+  } catch (error) {
+    log.error("CHAT-MODEL", "Error message: %j", error);
+    throw error;
+  }
+};
+
 module.exports = {
   selectMessagesByRoom,
 };
