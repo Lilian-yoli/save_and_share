21014cc84cc4da7cd2df8a1204483b5da4780979
LilianYang <lilianyang@emq.com>
2023-01-26 20:11:16 +0800

    SSB-0032: adjust chat-message api to chat-records

--- backend/controllers/chat_controller.js
+++ backend/controllers/chat_controller.js
@@ -1,27 +1,77 @@
 const { selectMessagesByRoom } = require("../models/chat_model");
+const { selectUserById } = require("../user/model");
+const log = require("npmlog");
 
-const chatMessagesFlow = async (req, res) => {
-  const { receiverId } = req.query;
-  const senderId = req.user.id;
-  const room = generateChatRoom(senderId, receiverId);
-  const historyMessages = await selectMessagesByRoom(room);
+const chatRecordsFlow = async (req, res) => {
+  const { theOtherUserId, room } = req.query;
+  console.log({ room: room });
+  const otherUserIdFromDb = await selectUserById(theOtherUserId);
+  if (otherUserIdFromDb.length !== 1) {
+    return res.status(422).send({ error: "No accessibility." });
+  }
+  const roomFormatChecker = checkRoomFormat(room);
+  if (roomFormatChecker.error) {
+    return res.status(422).send(roomFormatChecker);
+  }
+  const myUserId = req.user.id;
+  const verifiedRoomIdObject = checkRoomAccess(room, myUserId, theOtherUserId);
+  console.log({ verifiedRoomIdObject: verifiedRoomIdObject });
+  if (verifiedRoomIdObject.error) {
+    return res
+      .status(422)
+      .send({ error: "Something went wrong when getting chat records." });
+  }
+  const verifiedRoomId = verifiedRoomIdObject.room;
+  const chatRecords = await selectMessagesByRoom(verifiedRoomId);
   const respondedChatInfo = {
-    receiver_id: parseInt(receiverId),
-    sender_id: senderId,
-    history_messages: historyMessages,
-    room,
+    other_user_id: parseInt(theOtherUserId),
+    my_user_id: myUserId,
+    chat_records: chatRecords,
   };
   return res.status(200).send({ data: respondedChatInfo });
 };
 
-const generateChatRoom = (senderId, receiverId) => {
-  if (senderId > receiverId) {
-    return receiverId + "&" + senderId + "room";
-  } else {
-    return senderId + "&" + receiverId + "room";
+const checkRoomFormat = (room) => {
+  const roomRegexFormat = /\d*[A][N][D]\d*/;
+  const foundRoom = room.match(roomRegexFormat);
+  if (foundRoom[0] == room) return room;
+  else return { error: "No accessibility." };
+};
+
+const checkRoomAccess = (room, myUserId, theOtherUserId) => {
+  try {
+    if (!room) {
+      return { roomId: generateChatRoom(myUserId, theOtherUserId) };
+    } else {
+      const roomUsers = room.split("AND");
+      const qualifiedUserIdFromRoom = roomUsers.filter(
+        (id) => parseInt(id) == myUserId || parseInt(id) == theOtherUserId
+      );
+      if (qualifiedUserIdFromRoom.length === 2) {
+        return { roomId: room };
+      } else {
+        return { error: "No accessibility." };
+      }
+    }
+  } catch (error) {
+    log.error("CHAT-CONTROLLER", "Error message: %j", error);
+    return { error: "Something went wrong when checking accessibility." };
   }
 };
 
+const generateChatRoom = (senderId, receiverId) => {
+  if (senderId > receiverId) {
+    return receiverId + "AND" + senderId;
+  } else {
+    return senderId + "AND" + receiverId;
+  }
+};
+
+// const getChatUserListFlow = (req, res) => {
+//   const myUserId = req.user.id
+//   const
+// }
+
 module.exports = {
-  chatMessagesFlow,
+  chatRecordsFlow,
 };
